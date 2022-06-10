c4f73b46c1abbee9c6de35a6c1bc7fcbcd4124a3
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-10 00:52:48 -0700

    Added functionality for text color based on temp range

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+const colorCoding = () => {
+    const colorTemp = document.getElementById("tempContainer");
+
+    if (colorTemp.textContent >= 80) {
+        colorTemp.style.color = "red";
+    } else if (colorTemp.textContent  >= 70 && colorTemp.textContent <= 79) {
+        colorTemp.style.color = "orange";
+    } else if (colorTemp.textContent >= 60 && colorTemp.textContent <= 69) {
+        colorTemp.style.color = "yellow";
+    } else if (colorTemp.textContent >= 50 && colorTemp.textContent <= 59) {
+        colorTemp.style.color = "green";
+    } else {
+        colorTemp.style.color = "teal";
+    }
+};
+
+
 const state = {
     currentTemp: 70,
 };
@@ -6,12 +23,14 @@ const increaseTemp = () => {
     state.currentTemp += 1;
     const tempContainer = document.querySelector("#tempContainer");
     tempContainer.textContent = `${state.currentTemp}`;
+    colorCoding();
 }
 
 const decreaseTemp = () => {
     state.currentTemp -= 1;
     const tempContainer = document.querySelector("#tempContainer");
     tempContainer.textContent = `${state.currentTemp}`;
+    colorCoding();
 }
 
 const registerEventHandlers = () => {
