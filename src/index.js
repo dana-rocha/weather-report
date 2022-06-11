fb8e91364d250fdb7f5e5a97f00f6a355a4cc188
Vida Maleki <vida.basmani@gmail.com>
2022-06-11 09:25:24 -0700

    created city functions, sky functon , api

--- src/index.js
+++ src/index.js
@@ -1,78 +1,132 @@
-const colorCoding = () => {
-    const colorTemp = document.getElementById("tempContainer");
-
-    if (colorTemp.textContent >= 80) {
-        colorTemp.style.color = "red";
-    } else if (colorTemp.textContent >= 70 && colorTemp.textContent <= 79) {
-        colorTemp.style.color = "orange";
-    } else if (colorTemp.textContent >= 60 && colorTemp.textContent <= 69) {
-        colorTemp.style.color = "yellow";
-    } else if (colorTemp.textContent >= 50 && colorTemp.textContent <= 59) {
-        colorTemp.style.color = "green";
-    } else {
-        colorTemp.style.color = "teal";
-    }
+let state = {
+    currentTemp = 70,
+    city: 'Seattle'
 };
 
-const weatherGarden = () => {
-    const gardenTemp = document.getElementById("tempContainer");
-
-    if (gardenTemp.textContent >= 80) {
-        gardenContainer.textContent = "🌵 🌵 🌵 🌵";
-    } else if (gardenTemp.textContent >= 70 && gardenTemp.textContent <= 79) {
-        gardenContainer.textContent = "🌸 🌿 🌼 ";
-    } else if (gardenTemp.textContent >= 60 && gardenTemp.textContent <= 69) {
-        gardenContainer.textContent = "🌾 🌾 🌾 ";
-    } else {
-        gardenContainer.textContent = "🌲 ⛄️";
-    }
-}
-
-
-const state = {
-    city: Seattle,
-    currentTemp: 70,
-};
+const cityID = document.getElementById('cityname');
+const displayName = document.getElementById('headerCityName');
+const displaySky = document.getElementById('skyPic');
+const skyType = document.getElementById('skyoptions');
+const reset = document.getElementById('reset');
+const defaultCity = 'Seattle';
 
 const increaseTemp = () => {
     state.currentTemp += 1;
-    const tempContainer = document.querySelector("#tempContainer");
-    tempContainer.textContent = `${state.currentTemp}`;
+    const tempContainer = document.querySelector('temperature')
+    tempContainer.textContent = `${currentTemp} ℉`;
     colorCoding();
-    weatherGarden();
 }
 
 const decreaseTemp = () => {
     state.currentTemp -= 1;
-    const tempContainer = document.querySelector("#tempContainer");
-    tempContainer.textContent = `${state.currentTemp}`;
+    const tempContainer = document.querySelector('temperature')
+    tempContainer.textContent = `${currentTemp} ℉`;
     colorCoding();
-    weatherGarden();
 }
 
+const getCurrentTemp = function() {
+    let latitude;
+    let longitude;
+
+    axios
+        .get('http://localhost:5000/location', { params: { q: city } })
+        .then((response) => {
+            latitude = response.data[0].lat;
+            longitude = response.data[0].lon;
+            axios
+                .get('http://localhost:5000/weather', {
+                    params: { lat: latitude, lon: longitude },
+                })
+                .then((response) => {
+                    const kelvin = response.data.current.temp;
+                    const fahrenheit = (9 / 5) * (kelvin - 273) + 32;
+                    temperature = Math.round(fahrenheit);
+                    newTemperature();
+                })
+                .catch((error) => {
+                    console.log('error');
+                });
+        })
+        .catch((error) => {
+            console.log('error :(');
+        });
+};
+
+const resetCity = () => {
+    state.city = 'Seattle';
+    document.querySelector('#cityname').value = '';
+    const curWeatherHeader = document.getElementById('headerCityName');
+    curWeatherHeader.textContent = 'Current Weather for ' + state.city;
+};
+
+
+const updateCity = () => {
+    const inputCity = document.querySelector('#cityname');
+    inputCity.addEventListener('change', updateValue);
+
+    const headerCityName = document.getElementById('#headerCityName');
+    headerCityName.textContent = inputCity;
+
+    function updateValue(x) {
+        city = x.target.value;
+        headerCityName.textContent = 'Current Weather for ' + city;
+        getCurrentTemp();
+    }
+};
+
+
+
+// changing temperature color and garden picture base on degree
+const colorCoding = () => {
+    let landscape = document.querySelector("#skyGarden");
+    let colorTemp = document.getElementById('tempContainer');
+
+    if (state.currentTemp >= 80) {
+        colorTemp.style.color = "red";
+        landscape.textContent = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂🍳';
+    } else if (state.currentTemp >= 70 && state.currentTemp <= 79) {
+        colorTemp.style.color = "orange";
+        landscape.textContent = '🌸🌿🌼__🌷🌻🌿 _☘️🌱 _🌻🌷 ';
+    } else if (state.currentTemp >= 60 && state.currentTemp <= 69) {
+        colorTemp.style.color = "yellow";
+        landscape.textContent = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+    } else if (state.currentTemp >= 50 && state.currentTemp <= 59) {
+        colorTemp.style.color = "green";
+        landscape.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+    } else {
+        colorTemp.style.color = "teal";
+        landscape.textContent = '⛄️⛄️⛄️⛄️⛄️⛄️⛄️⛄️⛄️⛄️⛄️⛄️⛄️';
+    }
+};
+
+const updateSky = (a) => {
+    const optionSky = e.target.value;
+
+    if (optionSky === '⛅️Cloudy☁️') {
+        displaySky.textContent = '☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️';;
+    } else if (optionSky === '🌈Sunny☀️') {
+        displaySky.textContent = '☁️ ☁️ ☁️ ☀️ ☁️ ☁️';
+    } else if (optionSky === '⛈Rainy☔️') {
+        displaySky.textContent = '🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧';
+    } else if (optionSky === '❄️Snowy☃️') {
+        displaySky.textContent = '🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨';
+    }
+};
+
+//registering events
 const registerEventHandlers = () => {
     const increaseButton = document.querySelector("#increase-button");
     increaseButton.addEventListener("click", increaseTemp);
 
     const decreaseButton = document.querySelector("#decrease-button");
     decreaseButton.addEventListener("click", decreaseTemp);
+
+    skyType.addEventListener('change', updateSky);
+
+    cityID.addEventListener('input', updateCity);
+
+    reset.addEventListener('click', resetCity);
+    getCurrentTemp();
 };
-
-// document.addEventListener("DOMContentLoaded", registerEventHandlers);
-
-// drop down menu for sky
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-        var myDropdown = document.getElementById("myDropdown");
-        if (myDropdown.classList.contains('show')) {
-            myDropdown.classList.remove('show');
-        }
-    }
-}
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
