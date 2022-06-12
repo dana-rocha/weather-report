1fc95a74f22ea69ae2f6912dbce5f5fa9edd082a
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-12 11:21:33 -0700

    Fixed temp update based on click to reset

--- src/index.js
+++ src/index.js
@@ -71,10 +71,12 @@ const updateCurrentTemp = function(temp) {
 
 const resetCity = () => {
     state.city = 'Seattle';
+    state.currentTemp = 70;
     document.querySelector('#cityName').value = '';
     const curWeatherHeader = document.getElementById('headercityName');
     curWeatherHeader.textContent = 'Current Weather for ' + state.city;
-    console.log("Resetting!");
+    updateCurrentTemp(state.currentTemp);
+    // console.log("Resetting!");
 };
 
 
